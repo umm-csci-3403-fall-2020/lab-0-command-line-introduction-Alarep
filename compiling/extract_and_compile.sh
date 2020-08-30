2676c5b12b3dafd8c5df4d0ca3e346f2f1fb15ab
Jack Perala <peralaj94@gmail.com>
2020-08-30 12:27:17 -0500

    Compiled and linked the .c files.

---
+++ compiling/extract_and_compile.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# decompressing the tar file
+tar -zxf NthPrime.tgz
+
+# combining the .c files
+cd NthPrime
+gcc -c main.c nth_prime.c
+gcc main.o nth_prime.o -o NthPrime
+./NthPrime $1
